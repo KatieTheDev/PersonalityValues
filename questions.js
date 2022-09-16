9ee815eb236cf5b1f14ec5138911a6fc19d895af
KatieTheDev <51092829+KatieTheDev@users.noreply.github.com>
2022-09-16 14:03:54 -0600

    Change names of some elements

--- questions.js
+++ questions.js
@@ -2,120 +2,120 @@ questions = [
     {
         "question": "If one person is on one the side of a track and a train is coming at five people on the other side, I would pull the lever to kill the one instead of the five.",
         "effect": {
-            "econ": 25,
-            "dipl": 0
+            "stab": 25,
+            "karm": 0
         }
     },
     {
         "question":"Do you crack your knuckles?",
         "effect": {
-            "econ": 5,
-            "dipl": 0
+            "stab": 5,
+            "karm": 0
         }
     },
     {
         "question":"I would snitch on a friend who stole a soda from a store.",
         "effect": {
-            "econ": 15,
-            "dipl": 0
+            "stab": 15,
+            "karm": 0
         }
     },
     {
         "question":"If given the chance, I would press a button to kill a random person around the world in order to get ten million dollars in return.",
         "effect": {
-            "econ": 0,
-            "dipl": 35
+            "stab": 0,
+            "karm": 35
         }
     },
     {
         "question":"I would kill a random person by the press of a button, if it means the slums around the world would be fed for a week.",
         "effect": {
-            "econ": 30,
-            "dipl": 0
+            "stab": 30,
+            "karm": 0
         }
     },
     {
         "question":"I would kill two random people to save your best friend's life.",
         "effect": {
-            "econ": 50,
-            "dipl": 0
+            "stab": 50,
+            "karm": 0
         }
     },
     {
         "question":"It is better to forgive someone than to hurt them if they wrong you.",
         "effect": {
-            "econ": 20,
-            "dipl": -20
+            "stab": 20,
+            "karm": -20
         }
     },
     {
         "question":"I would hit someone if they hit me first instead of letting it go.",
         "effect": {
-            "econ": -15,
-            "dipl": 15
+            "stab": -15,
+            "karm": 15
         }
     },
     {
         "question":"If I had superpowers, I would use them for good instead of evil/my own devices.",
         "effect": {
-            "econ": 15,
-            "dipl": -5
+            "stab": 15,
+            "karm": -5
         }
     },
     {
         "question":"I would throw away a cure to cancer in order to get billions of dollars.",
         "effect": {
-            "econ": -5,
-            "dipl": 45
+            "stab": -5,
+            "karm": 45
         }
     },
     {
         "question":"I agree with Batman's \"No killing rule\".",
         "effect": {
-            "econ": 0,
-            "dipl": -25
+            "stab": 0,
+            "karm": -25
         }
     },
     {
         "question":"I see myself as a force of nature, rather than a variable.",
         "effect": {
-            "econ": 30,
-            "dipl": 0
+            "stab": 30,
+            "karm": 0
         }
     },
     {
         "question":"I believe world peace is possible and desirable.",
         "effect": {
-            "econ": 0,
-            "dipl": -30
+            "stab": 0,
+            "karm": -30
         }
     },
     {
         "question":"If given the chance, I would rather donate change than save for a vacation or personal expense.",
         "effect": {
-            "econ": 10,
-            "dipl": -15
+            "stab": 10,
+            "karm": -15
         }
     },
     {
         "question":"I follow logic over feelings/I am not influenced by my own feelings.",
         "effect": {
-            "econ": 25,
-            "dipl": 5
+            "stab": 25,
+            "karm": 5
         }
     },
     {
         "question":"I would help a villian if it would give me power.",
         "effect": {
-            "econ": -10,
-            "dipl": 35
+            "stab": -10,
+            "karm": 35
         }
     },
     {
         "question":"I would save someone if they were being beat up.",
         "effect": {
-            "econ": 0,
-            "dipl": -30
+            "stab": 0,
+            "karm": -30
         }
     }
 ];
