a075c386755f8f24cd16bea60138c7337eec1ac7
KatieTheDev <51092829+KatieTheDev@users.noreply.github.com>
2022-09-16 13:58:51 -0600

    Fix wording of a question

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        "question": "If one person is on one the side of a track and a train is coming at five, I would pull the level to kill the one instead of the five.",
+        "question": "If one person is on one the side of a track and a train is coming at five people on the other side, I would pull the lever to kill the one instead of the five.",
         "effect": {
             "econ": 25,
             "dipl": 0
