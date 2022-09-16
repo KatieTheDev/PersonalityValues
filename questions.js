7f71b478685d818a2c58fb09a7e92cdb76e97fcc
KatieTheDev <51092829+KatieTheDev@users.noreply.github.com>
2022-09-16 12:33:20 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,17 +1,36 @@
 questions = [
     {
-        "question": "If one person is on one the side of a track and a train is coming at five, do you pull the level to kill the one instead of the five.",
+        "question": "If one person is on one the side of a track and a train is coming at five, I would pull the level to kill the one instead of the five.",
         "effect": {
             "econ": 25,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
+            "dipl": 0
         }
     },
     {
         "question":"Do you crack your knuckles?",
         "effect": {
             "econ": 5,
+            "dipl": 0
+        }
+    },
+    {
+        "question":"I would snitch on a friend who stole a soda from a store.",
+        "effect": {
+            "econ": 15,
+            "dipl": 0
+        }
+    },
+    {
+        "question":"If given the chance, I would press a button to kill a random person around the world in order to get ten million dollars in return.",
+        "effect": {
+            "econ": 0,
+            "dipl": -35
+        }
+    },
+    {
+        "question":"I would kill a random person by the press of a button, if it means the slums around the world would be fed for a week.",
+        "effect": {
+            "econ": 30,
             "dipl": 0
         }
     }
